6a8376ea0f4de3e7c2f5ca26ddad80f624c29159
lt616 <qz2338@columbia.edu>
2018-09-06 10:59:31 -0400

    LinkedList & Hash

--- lru_cache_134.java
+++ lru_cache_134.java
@@ -7,6 +7,10 @@ get(key) - Get the value (will always be positive) of the key if the key exists 
 set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item. 
 */ 
 
+/* 
+	EASY WRONG POINTS: 
+		
+
 class Node { 
     int key; 
     int val; 
