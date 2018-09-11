187c7749497fd9372f129ece23da0f3295f6d2a0
lt616 <qz2338@columbia.edu>
2018-09-10 22:13:26 -0400

    note

---
+++ templates/sliding_window.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: an integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+        int j = 0, i = 0;
+        int sum =0;
+        int ans = Integer.MAX_VALUE;
+        for(i = 0; i < nums.length; i++) {
+            while(j < nums.length && sum < s) {
+                sum += nums[j];
+                j ++;
+            }
+            if(sum >=s) {
+                ans = Math.min(ans, j - i);
+            }
+            sum -= nums[i];
+        }
+        if(ans == Integer.MAX_VALUE)
+            ans = -1;
+        return ans;
+    }
+} 
