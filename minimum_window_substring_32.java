504eff9eadd369fb885cea8d6829aebaf6879439
lt616 <qz2338@columbia.edu>
2018-10-23 12:03:11 -0400

    Sliding window

--- minimum_window_substring_32.java
+++ minimum_window_substring_32.java
@@ -76,4 +76,50 @@ public class Solution {
         
         return source.substring(start, end);  
     }
+}  
+
+
+/* Solution 02: Improved Sliding Window */ 
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+            return ""; 
+        } 
+        
+        int[] char_num = new int[150]; 
+        Set<Character> t_set = new HashSet<Character>(); 
+        for (int i = 0;i < t.length();i ++) { 
+            char_num[t.charAt(i) - 'A'] ++; 
+            t_set.add(t.charAt(i)); 
+        }
+        
+        int j = 0, min_len = Integer.MAX_VALUE, start = -1, end = -1; 
+        for (int i = 0;i < s.length();i ++) {
+            while (j < s.length() && ! t_set.isEmpty()) {
+                char c = s.charAt(j); 
+                char_num[c - 'A'] --; 
+                
+                if (t_set.contains(c)) { 
+                    if (char_num[c - 'A'] == 0) {
+                        t_set.remove(c); 
+                    } 
+                } 
+                j ++; 
+            } 
+            
+            if (j <= s.length() && t_set.isEmpty() && j - i < min_len) { 
+                min_len = j - i; 
+                start = i; 
+                end = j; 
+            } 
+            
+            char c = s.charAt(i); 
+            char_num[c - 'A'] ++; 
+            if (char_num[c - 'A'] == 1) {
+                t_set.add(c); 
+            }
+        } 
+        
+        return (start == -1) ? "" : s.substring(start, end); 
+    }
 } 
