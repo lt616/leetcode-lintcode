a267b32dd67108576e4971913d1fa1b645af2f6d
肇小狼 <qz2338@columbia.edu>
2019-07-07 21:46:25 -0700

    bi BFS

--- 127_word_ladder.java
+++ 127_word_ladder.java
@@ -38,3 +38,84 @@ class Solution {
         return diff;
     }
 }
+
+/* Solution 02: Bidirectional BFS */
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        /* If endWord is not in the list */
+        int num = 0;
+        for (int i = 0; i < wordList.size(); i++) {
+            if (wordList.get(i).equals(endWord)) {
+                num ++;
+            }
+        }
+        
+        if (num != 1) {
+            System.out.println("match");
+            return 0;
+        }
+        
+        Queue<String> queueForward = new LinkedList<>();
+        Queue<String> queueBackward = new LinkedList<>();
+        
+        int level= 0;
+        
+        Set<String> setForward = new HashSet<>();
+        Set<String> setBackward = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        
+        queueForward.add(beginWord);
+        setForward.add(beginWord);
+        queueBackward.add(endWord);
+        setBackward.add(endWord);
+        
+        while (!queueForward.isEmpty() || !queueBackward.isEmpty()) {
+            int levelSizeForward = queueForward.size();
+            if (levelSizeForward != 0) level ++;
+            for (int i = 0; i < levelSizeForward; i++) {
+                String currentWord = queueForward.poll();
+                if (visited.contains(currentWord)) {
+                    return (visited.contains(currentWord)) ? level - 1 : level;
+                }
+                visited.add(currentWord);
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (!setForward.contains(wordList.get(j)) 
+                        && !visited.contains(wordList.get(j))
+                        && wordDistance(wordList.get(j), currentWord) == 1) {                        
+                        queueForward.add(wordList.get(j));
+                        setForward.add(wordList.get(j));
+                    }
+                }
+            }
+            
+            int levelSizeBackward = queueBackward.size();
+            if (levelSizeBackward != 0) level ++;
+            for (int i = 0; i < levelSizeBackward; i++) {
+                String currentWord = queueBackward.poll();
+                if (visited.contains(currentWord)) {
+                    return (visited.contains(currentWord)) ? level - 1 : level;
+                }
+                visited.add(currentWord);
+                
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (!setBackward.contains(wordList.get(j))
+                        && !visited.contains(wordList.get(j))
+                        && wordDistance(wordList.get(j), currentWord) == 1) {
+                        queueBackward.add(wordList.get(j));
+                        setBackward.add(wordList.get(j));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+    
+    private int wordDistance(String word, String target) {
+        int diff = 0;
+        for (int i = 0;i < word.length(); i++) {
+            if (word.charAt(i) != target.charAt(i))
+                diff ++;
+        }
+        return diff;
+    }
+}
